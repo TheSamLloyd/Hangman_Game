d2eba54e32d0e7df47c6f78739058a91c4e5eab9
TheSamLloyd <samlloydj@gmail.com>
2018-01-03 19:46:30 -0500

    added theme and more functionality. still not 100% but that's how it is on this bitch of an earth

--- hangman.js
+++ hangman.js
@@ -1,32 +1,38 @@
 var wordlist = ["astronaut", "space", "moon", "planet","antimatter","asteroid","meteor","mars","constellation","star"];
 var letters = "abcdefghijklmnopqrstuvwxyz";
 var wins = 0;
-var game = {
-	guesses:12,
-	attempts:[],
+function gameStart(){
+	this.guesses=12;
+	this.attempts=[];
 // steps:
 // generate a random word from a list
-	secretword: wordlist[Math.floor(Math.random()*wordlist.length)],
+	this.secretword=wordlist[Math.floor(Math.random()*wordlist.length)];
 // listen for keypresses
+		var blankDiv = document.getElementById("blanksList");
+	for (var i=0;i<this.secretword.length;i++){
+		var newBlank=document.createElement("li");
+		newBlank.innerHTML="_";
+		newBlank.setAttribute("value",this.secretword[i]);
+		newBlank.setAttribute("revealed","false");
+		newBlank.setAttribute("class","blank");
+		blankDiv.appendChild(newBlank);
+	}
 };
-console.log(game);
-var blankDiv = document.getElementById("blanksList")
-for (var i=0;i<game.secretword.length;i++){
-	var newBlank=document.createElement("li");
-	newBlank.innerHTML="_";
-	newBlank.setAttribute("value",game.secretword[i]);
-	newBlank.setAttribute("class","blank");
-	blankDiv.appendChild(newBlank);
-}
+var game = new gameStart();
 var blanks = document.getElementsByClassName("blank");
 document.addEventListener("keydown",function(){
+	var win=true;
 	console.log(event.key)
 	var inword=false;
-	var revCount=0;
 	for(var i=0;i<blanks.length;i++){
 		if(blanks[i].getAttribute("value")===event.key){
 			blanks[i].innerHTML=blanks[i].getAttribute("value");
+			blanks[i].setAttribute("revealed","true");
 			inword=true;
+		}
+		if (blanks[i].getAttribute("revealed")=="false"){
+			console.log(blanks[i].getAttribute("revealed"))
+			win=false;
 		}
 	}
 	if (inword==false && 
@@ -45,4 +51,12 @@ document.addEventListener("keydown",function(){
 			}
 		}
 	}
+	if (win){
+		document.getElementById("status-text").innerHTML="You win!";
+		document.getElementById("wincount").innerHTML=++wins;
+	}
 })
+function restart(){
+	game = gameStart();
+	
+}
